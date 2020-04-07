e198d7cda99fb8ff6cbced3c42fe6638d7114d21
Marek Ištvánek <istvanek@hasam.cz>
2020-04-07 08:42:36 +0200

    IConnectionManager quotation extensions

--- ETLBox/src/Definitions/ConnectionManager/ConnectionManagerSpecifics.cs
+++ ETLBox/src/Definitions/ConnectionManager/ConnectionManagerSpecifics.cs
@@ -1,6 +1,10 @@
-﻿namespace ALE.ETLBox.ConnectionManager
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ALE.ETLBox.ConnectionManager
 {
-    public class ConnectionManagerSpecifics
+    public static class ConnectionManagerSpecifics
     {
         public static ConnectionManagerType GetType(IConnectionManager connection)
         {
@@ -23,7 +27,9 @@
             else return ConnectionManagerType.Unknown;
         }
 
-        public static string GetBeginQuotation(ConnectionManagerType type)
+        public static ConnectionManagerType Type(this IConnectionManager connection) => GetType(connection);
+
+        public static string GetBeginQuotation(this ConnectionManagerType type)
         {
             if (type == ConnectionManagerType.SqlServer || type == ConnectionManagerType.Access)
                 return @"[";
@@ -35,7 +41,7 @@
                 return string.Empty;
         }
 
-        public static string GetEndQuotation(ConnectionManagerType type)
+        public static string GetEndQuotation(this ConnectionManagerType type)
         {
             if (type == ConnectionManagerType.SqlServer || type == ConnectionManagerType.Access)
                 return @"]";
@@ -43,6 +49,30 @@
                 return GetBeginQuotation(type);
         }
 
+        public static string AddQuotations(this ConnectionManagerType type, string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Value cannot be null or white space", nameof(name));
+            string qb = type.GetBeginQuotation();
+            string qe = type.GetEndQuotation();
+            if (!name.StartsWith(qb))
+                name = qb + name;
+            if (!name.EndsWith(qe))
+                name += qe;
+            return name;
+        }
+
+        public static IEnumerable<string> AddQuotations(this ConnectionManagerType type, IEnumerable<string> names)
+        {
+            if (names is null)
+                throw new ArgumentNullException(nameof(names));
+            return names.Select(i => type.AddQuotations(i));
+        }
+
+        public static string AddQuotations(this IConnectionManager connection, string name) => connection.Type().AddQuotations(name);
+
+        public static IEnumerable<string> AddQuotations(this IConnectionManager connection, IEnumerable<string> names) =>
+            connection.Type().AddQuotations(names);
 
         public static string GetBeginQuotation(IConnectionManager connectionManager) => GetBeginQuotation(GetType(connectionManager));
         public static string GetEndQuotation(IConnectionManager connectionManager) => GetEndQuotation(GetType(connectionManager));
